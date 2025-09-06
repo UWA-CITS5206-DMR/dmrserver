618231d202c22b4254b264360243d88ee2efab96
phoever23 <gaoming93@foxmail.com>
2025-09-06 18:14:16 +0800

    Update CORS settings in dmr/settings.py to specify allowed headers and methods

--- dmr/settings.py
+++ dmr/settings.py
@@ -142,8 +142,25 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_HEADERS = "*"
-
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 SPECTACULAR_SETTINGS = {
     "TITLE": "DMR API",
     "DESCRIPTION": "DMR API documentation",
