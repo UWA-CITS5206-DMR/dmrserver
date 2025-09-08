ce189a8df39fcdd26e01c89cb1aa6ee5e8f1363e
Samuel <zimingw.ol@outlook.com>
2025-09-08 22:02:35 +0800

    feat: add Dockerfile for production deployment with gunicorn and uv

--- dmr/settings.py
+++ dmr/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Media files (user uploads)
 MEDIA_URL = "/media/"
